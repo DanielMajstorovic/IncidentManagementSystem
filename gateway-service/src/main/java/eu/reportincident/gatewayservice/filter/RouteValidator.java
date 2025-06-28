fa89b38490764f3e16f6472251cfe783e7bc76cf
da-ni-e-l <116642398+DanielMajstorovic@users.noreply.github.com>
2025-06-28 12:41:16 +0200

    Update RouteValidator.java

--- gateway-service/src/main/java/eu/reportincident/gatewayservice/filter/RouteValidator.java
+++ gateway-service/src/main/java/eu/reportincident/gatewayservice/filter/RouteValidator.java
@@ -2,6 +2,7 @@ package eu.reportincident.gatewayservice.filter;
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -9,10 +10,13 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
 
+    private static final AntPathMatcher pathMatcher = new AntPathMatcher();
+
     // Lista putanja koje su otvorene i ne zahtevaju JWT token
     public static final List<String> openApiEndpoints = List.of(
             "/auth-service/oauth2/authorization/google",
             "/auth-service/login/oauth2/code/google",
+            "/incident-service/api/v1/incidents/image-url/**",
             "/incident-service/api/v1/incidents/filter" // Pretpostavka da je filter za anonimne
     );
 
@@ -20,5 +24,5 @@ public class RouteValidator {
     public Predicate<ServerHttpRequest> isSecured =
             request -> openApiEndpoints
                     .stream()
-                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+                    .noneMatch(uri -> pathMatcher.match(uri, request.getURI().getPath()));
 }
